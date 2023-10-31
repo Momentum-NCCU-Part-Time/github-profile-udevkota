fb86abfd0077003c5851b4f5db5237efff913cf4
Upasana Devkota <devkotaupasana@gmail.com>
2023-10-31 16:57:09 -0400

    IP Added JS code for repo.

--- main.js
+++ main.js
@@ -4,20 +4,10 @@ let sumbitBtn = document.querySelector('button')
 sumbitBtn.addEventListener('click', (event) => {
   event.preventDefault()
   let usernameInputVal = document.querySelector('input').value
-  let githubUrl = 'https://api.github.com/users/'
 
-
-
-  fetch(githubUrl+usernameInputVal)
+  fetch(`https://api.github.com/users/${usernameInputVal}`)
     .then(res => res.json())
     .then(data => {
-      // console.log(data)
-      // console.log(data['name'])
-      // console.log(data['avatar_url'])
-      // console.log(data['location'])
-      console.log(data['html_url'])
-      // console.log(data['login'])
-
       let userInfo = `<div class="userImageAndName">
                         <img src="${data['avatar_url']} class="userImage">
                         <h2 class="userName">${data['name']}</h2>
@@ -30,31 +20,25 @@ sumbitBtn.addEventListener('click', (event) => {
                       <div class="repos">
                         <h3 class="reposTitle">GitHub Repos</h3>
                         <ul class="reposList">
-                        </ul>
+                        </ul class="listOfRepos">
                       </div>`
 
-                    
-      
+
       fetch(`https://api.github.com/users/${usernameInputVal}/repos`)
         .then(res => res.json())
         .then(data => {
-          for(let i=0; i < data.length; i++){
-            let repoLi = `<span>${data[1]['html_url']}</span>
-            <span>${data[1]['name']}</span>`
+          console.log(data)
+
+          function customerToHTML(repoData) {
+            return `<li><a href="${repoData['html_url']}">${repoData['name']}</a></li>`
+                    
           }
 
-          function repoDataToHTML(repository){
-            // console.log(data[1]['name'])
-            // console.log(data[1]['html_url'])
-            let repoLi = `<span>${data[1]['html_url']}</span>
-            <span>${data[1]['name']}</span>`
-            // let repoLi = `<li><a href="${data['html_url']}">${data['name']}</a></li>`
+          let allRepos = data.map((repoElement) => customerToHTML(repoElement)).join('\n');
 
-            entryPoint.innerHTML = userInfo + repoLi
-          }
-          
-
+          entryPoint.innerHTML = userInfo + allRepos
         })
+      
     })
     .catch( err => {
       console.log(err)
@@ -63,6 +47,6 @@ sumbitBtn.addEventListener('click', (event) => {
 
 
 //Need to do 
-// run through array of repos and make them appear 
 // fix submit button to input 
+// css
 // add icons
